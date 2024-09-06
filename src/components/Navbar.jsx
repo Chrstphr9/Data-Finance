7d18eedc03805ea0b9390666a8cb7472d743b306
somto <somto565@gmail.com>
2024-09-06 10:47:19 +0200

    menu button working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+      setNav(!nav)
+    }
+
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACt</h1>
@@ -12,10 +18,11 @@ const Navbar = () => {
             <li className='p-4'>Resources</li>
             <li className='p-4'>Contact</li>
         </ul>
-        <div>
-            <AiOutlineMenu size={20} />
+        <div onClick={handleNav}>
+          {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         </div>
-        <div className='fixed left-0 top-0 h-full w-[60%] border-r border-r-gray-900 bg-[#000020]'>
+
+        <div className={!nav ? 'fixed left-0 top-0 h-full w-[60%] border-r border-r-gray-900 bg-[#000020] ease-in-out duration-500': 'fixed left-[-1000%]'}>
         <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACt</h1>
           <ul className='uppercase p-4'>
             <li className='p-4 border-b border-gray-600'>Home</li>
